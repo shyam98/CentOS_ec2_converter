490189eca3917086343de41d74b744e106e39b9e
Shyam Patel <shyam98@gmail.com>
2019-05-22 13:22:35 -0700

    Update TestConverter.py

--- TestConverter.py
+++ TestConverter.py
@@ -9,9 +9,9 @@ import time
 
 input_filepath = '~/home/centos/CentOS_ec2_converter/modern.fbx'
 
-convert_stl_fbx_to_obj_gltf(input_filepath)
+convert_gltf(input_filepath)
 
-def convert_stl_fbx_to_obj_gltf(input_filepath):
+def convert_gltf(input_filepath):
     print("convert_stl_fbx_to_obj:Calling blender to convert stl/fbx file:" + input_filepath)
     cmdline = "/home/centos/blender-2.8/blender -b --python ObjGltf.py -- {0}" \
         .format(input_filepath)

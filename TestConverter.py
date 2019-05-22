fecbaf36a0c80f4ceb61cc105d22c287c23cfa2e
Shyam Patel <shyam98@gmail.com>
2019-05-22 13:31:34 -0700

    Update TestConverter.py

--- TestConverter.py
+++ TestConverter.py
@@ -10,12 +10,9 @@ import time
 input_filepath = '~/home/centos/CentOS_ec2_converter/modern.fbx'
 
 print("convert_stl_fbx_to_obj:Calling blender to convert stl/fbx file:" + input_filepath)
+
 cmdline = "/home/centos/blender-2.8/blender -b --python ObjGltf.py -- {0}" \
     .format(input_filepath)
 print("convert_stl_fbx_to_obj: blender command: " + cmdline)
-p = subprocess.Popen(cmdline, shell=True, stdout=subprocess.PIPE)
-print("convert_stl_fbx_to_obj: blender subprocess starting")
-out, err = p.communicate()
-if p.returncode != 0:
-    print("Blender failed: stl/fbx to obj conversion failed %d %s" % (p.returncode, out))
-    raise Exception('Blender Failed')
+
+os.system(cmdline)

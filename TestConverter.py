9f366649481cd3ea48544b26740dcbc405ddde4e
Shyam Patel <shyam98@gmail.com>
2019-05-22 14:09:02 -0700

    Update TestConverter.py

--- TestConverter.py
+++ TestConverter.py
@@ -7,8 +7,8 @@ import sys
 import time
 
 
-cmd_fbx_gltf = "./home/centos/blender-2.8/blender -b --python ObjGltf.py"
+cmd_fbx_gltf = "./home/centos/blender-2.80/blender -b --python ObjGltf.py"
 os.system(cmd_fbx_gltf)
 
-#cmd_gltf_usdz = "python /home/centos/gltf2usd/Source/gltf2usd.py -g /home/centos/CentOS_ec2_converter/Modern.gltf -o /home/centos/CentOS_ec2_converter/Modern.usdz"
-#os.system(cmd_gltf_usdz)
+cmd_gltf_usdz = "python /home/centos/gltf2usd/Source/gltf2usd.py -g /home/centos/CentOS_ec2_converter/Modern.gltf -o /home/centos/CentOS_ec2_converter/Modern.usdz"
+os.system(cmd_gltf_usdz)
